b53e6f3e2e246d3c8196992e41c9f80e687d2dd2
Philipp Schrader <philipp.schrader@nuvation.com>
2013-07-23 12:40:02 -0700

    Adding easymotion to my vim plugin list.

--- install.sh
+++ install.sh
@@ -103,3 +103,4 @@ install_vim_plugin "ultisnips" https://github.com/SirVer/ultisnips.git
 install_vim_plugin "surround" https://github.com/tpope/vim-surround.git
 install_vim_plugin "ack.vim" https://github.com/mileszs/ack.vim.git
 install_vim_plugin "gundo.vim" https://github.com/sjl/gundo.vim.git
+install_vim_plugin "vim-easymotion" https://github.com/svermeulen/vim-easymotion.git
