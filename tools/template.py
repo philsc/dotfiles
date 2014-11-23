80c45076f1a4be715f57afaa687fdfb43a7c0d2f
Philipp Schrader <philipp.schrader@gmail.com>
2014-11-23 12:53:24 -0800

    templates: Confirm before instantiating in non-empty folder.

--- tools/template.py
+++ tools/template.py
@@ -17,7 +17,21 @@ def main(mainargs):
     else:
         srcdir = os.path.join(os.environ['HOME'], '.templates', args.template)
         destdir = os.path.join(os.getcwd(), '.')
-        distutils.dir_util.copy_tree(srcdir, destdir)
+        perform_copy = True
+
+        if os.listdir(destdir):
+            prompt = "Current directory isn't empty. Continue? [y/n] "
+
+            try:
+                answer = raw_input(prompt)
+            except NameError:
+                answer = input(prompt)
+
+            if answer[0].lower() != 'y':
+                perform_copy = False
+
+        if perform_copy:
+            distutils.dir_util.copy_tree(srcdir, destdir)
 
 if __name__ == '__main__':
     main(sys.argv)
