27d59cae37fd10472558794ce15f230c201ca40b
Philipp Schrader <philipp.schrader@gmail.com>
2014-11-23 12:26:00 -0800

    templates: Fix the template instantiation.
    Templates now get copied properly into the current directory.

--- tools/template.py
+++ tools/template.py
@@ -2,19 +2,22 @@
 
 import os
 import sys
-import distutils
+import distutils.dir_util
 import argparse
 
 def main(mainargs):
     parser = argparse.ArgumentParser()
     parser.add_argument('template', help='Which template to instantiate.')
+    parser.add_argument('--list', '-l', action='store_true', \
+            dest='list_templates', help='List the available templates.')
+    args = parser.parse_args(mainargs[1:])
 
-    args = parser.parse(mainargs)
-
-    srcdir = os.path.join(os.environ('HOME'), '.templates', args.template)
-    destdir = os.path.join(os.getcwd(), '.')
-
-    distutils.dir_util.copy_tree(srcdir, destdir)
+    if args.list_templates:
+        sys.stderr.write('TODO: Implement listing templates.\n')
+    else:
+        srcdir = os.path.join(os.environ['HOME'], '.templates', args.template)
+        destdir = os.path.join(os.getcwd(), '.')
+        distutils.dir_util.copy_tree(srcdir, destdir)
 
 if __name__ == '__main__':
     main(sys.argv)
