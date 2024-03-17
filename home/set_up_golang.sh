df8dcca4dfddfe503464a3aab184698ae22a8d15
Philipp Schrader <philipp.schrader@gmail.com>
2024-03-17 11:43:11 -0700

    home: Update Go to 1.21.0

--- home/set_up_golang.sh
+++ home/set_up_golang.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly URL="https://go.dev/dl/go1.18.1.linux-amd64.tar.gz"
+readonly URL="https://go.dev/dl/go1.21.0.linux-amd64.tar.gz"
 
 TEMPDIR="$(mktemp -d)"
 readonly TEMPDIR
