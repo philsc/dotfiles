74a1e62820d9c312f7c558595375c976218e7494
Philipp Schrader <philipp.schrader@gmail.com>
2026-09-15 07:19:25 +0100

    home: go: Update to 1.27.1 and add aarch64 support

--- home/set_up_golang.sh
+++ home/set_up_golang.sh
@@ -4,7 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly URL="https://go.dev/dl/go1.26.0.linux-amd64.tar.gz"
+readonly GO_VERSION="1.27.1"
+
+case "$(uname -m)" in
+  x86_64)  readonly GO_ARCH="amd64" ;;
+  aarch64) readonly GO_ARCH="arm64" ;;
+  *) echo "Unsupported architecture: $(uname -m)" >&2; exit 1 ;;
+esac
+
+readonly URL="https://go.dev/dl/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz"
 
 TEMPDIR="$(mktemp -d)"
 readonly TEMPDIR
